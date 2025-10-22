236ba2a837e293896c7752ccd9c56280336684aa
PasinduDilshan99 <pd.dimbulana@gmail.com>
2025-10-22 21:39:17 +0530

    add

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import TourPageMap from "@/components/sri-lankan-tours-components/tour-map-components/TourPageMap";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <TourPageMap />
+    </div>
+  );
+};
+
+export default page;
